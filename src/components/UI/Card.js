44dc14229a8fb25332f59d1aad10ba88902284b7
Ani <jancso.anita@gmail.com>
2021-08-27 16:55:22 +0200

    refactor: delete unused code

--- src/components/UI/Card.js
+++ src/components/UI/Card.js
@@ -1,11 +1,6 @@
 import React from 'react';
 import classes from './Card.module.css'
-import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    Link
-  } from "react-router-dom";
+
 function Card(props){
     return <div className={classes.card} ></div>
 }
