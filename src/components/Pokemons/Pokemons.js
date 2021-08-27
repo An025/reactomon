c376fcb37a6bd1428cf4c7ba5bb9c8b21700d6e8
Ani <jancso.anita@gmail.com>
2021-08-27 17:52:59 +0200

    fetch data with axios

--- src/components/Pokemons/Pokemons.js
+++ src/components/Pokemons/Pokemons.js
@@ -1,7 +1,18 @@
-import React from 'react';
-import Container from '../UI/Container'
+import React, {useState, useEffect} from 'react';
+import Container from '../UI/Container';
+import axios from 'axios';
 function Pokemons() {
-    return <Container title={"Pokemon"}/>;
+    const[pokemons, setPokemons] = useState([]);
+
+    useEffect(()=>{
+      console.log("Hello axios");
+      axios.get("https://pokeapi.co/api/v2/pokemon")
+      .then(response => setPokemons(response.data.results))
+    }, [])
+    return <Container title={"Pokemon"}>
+
+    </Container>
+   
   }
 
 export default Pokemons;
